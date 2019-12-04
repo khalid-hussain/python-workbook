6a58b0df59717a6d4f5766c8bda9e49bd3763f0b
Mohammad Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2019-12-05 00:55:50 +0800

    Solved 15

---
+++ 15-distance-units.py
@@ -0,0 +1,10 @@
+measurement = int(input("Input measurement in feet: "))
+
+print('The measurement in inches is {:.2f}'
+  .format(measurement * 12))
+
+print('The measurement in yards is {:.2f}'
+  .format(measurement / 3))
+
+print('The measurement in miles is {:.2f}'
+  .format(measurement /5280))
