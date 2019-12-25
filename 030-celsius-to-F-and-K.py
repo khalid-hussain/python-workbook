b648fd58573533bd0589aaab02e74253a2aa8188
Mohammad Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2019-12-26 03:28:38 +0800

    Solved 30

---
+++ 030-celsius-to-F-and-K.py
@@ -0,0 +1,8 @@
+temp_C = float(input('Input temperature (C): '))
+
+temp_F = temp_C * 9 / 5 + 32
+
+temp_K = temp_C + 273.15
+
+print('Temperature in Fahrenheit is {:.2f}'.format(temp_F))
+print('Temperature in Kelvin is {:.2f}'.format(temp_K))
