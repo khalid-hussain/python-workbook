75028d746f8307aef86b114b785cbc95638f4410
Mohammad Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2019-12-26 07:25:22 +0800

    Solved 32

---
+++ 032-sum-of-digits-integer.py
@@ -0,0 +1,8 @@
+x = input('Input integer: ')
+
+sumOfDigits = int(x[0]) \
+            + int(x[1]) \
+            + int(x[2]) \
+            + int(x[3])
+
+print('The sum of the digits is {}.'.format(sumOfDigits))
