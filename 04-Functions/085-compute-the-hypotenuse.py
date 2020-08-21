dcfc9f2c8bc56b36d3c1238c4bdd3de2d5f17dc3
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2020-08-21 20:30:04 +0800

    Solved 85

--- 04-Functions/085-compute-the-hypotenuse.py
+++ 04-Functions/085-compute-the-hypotenuse.py
@@ -1,6 +1,10 @@
 from math import sqrt
 
 
+# Calculate the hypotenuse given two sides
+# @param a first side of triangle
+# @param b second side of triangle
+# @return c size of hypotenuse
 def getHypotenuse(a, b):
     return sqrt(a**2 + b**2)
 
