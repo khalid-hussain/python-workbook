d113fc1496a806add45f609ada24ac1fcd0173e8
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2020-08-22 12:44:00 +0800

    Solved 121

---
+++ 05-Lists/121-random-lottery-numbers.py
@@ -0,0 +1,20 @@
+import random
+
+
+def getSixRandomNumbers() -> list:
+    theNumbers = []
+    while len(theNumbers) < 6:
+        x = random.randrange(1, 50)
+        # print(f'x: {x}')
+        if x not in theNumbers:
+            theNumbers.append(x)
+
+    return theNumbers
+
+
+def main():
+    print(getSixRandomNumbers())
+
+
+if __name__ == "__main__":
+    main()
