dd33e541d1eaf83d8f4aa623b66e82cda6ecd0d0
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2020-08-22 11:22:17 +0800

    Added my library

---
+++ 05-Lists/khalid_lib.py
@@ -0,0 +1,19 @@
+# From Question 117
+def getWordsFromString(theString):
+    theWords = theString.split(' ')
+    theFinalWords = []
+    punctuationMarks = [',', '.', '?', '-', '\'', '!', ':', ';']
+    for word in theWords:
+        if word[-1] in punctuationMarks:
+            theFinalWords.append(word[:-1])
+        else:
+            theFinalWords.append(word)
+    return theFinalWords
+
+
+def main():
+    print("Main function")
+
+
+if __name__ == "__main__":
+    main()
