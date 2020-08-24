dce9dd177cc99dc118891cdc8cdf113713890d8a
Khalid Hussain <mohammad.khalid.hussain@gmail.com>
2020-08-24 15:24:09 +0800

    Used keys() method instead

--- 06-Dictionaries/143-anagrams.py
+++ 06-Dictionaries/143-anagrams.py
@@ -2,16 +2,16 @@ def isAnagram(strA: str, strB: str) -> bool:
     mDictOne = {}
     mDictTwo = {}
     for letter in strA:
-        if mDictOne.setdefault(letter, 0):
-            mDictOne[letter] = 1
-        else:
+        if letter in mDictOne.keys():
             mDictOne[letter] = mDictOne[letter] + 1
+        else:
+            mDictOne[letter] = 1
 
     for letter in strB:
-        if mDictTwo.setdefault(letter, 0):
-            mDictTwo[letter] = 1
-        else:
+        if letter in mDictTwo.keys():
             mDictTwo[letter] = mDictTwo[letter] + 1
+        else:
+            mDictTwo[letter] = 1
 
     if mDictOne == mDictTwo:
         return True
@@ -20,8 +20,8 @@ def isAnagram(strA: str, strB: str) -> bool:
 
 
 def main():
-    myStrA = 'listen'
-    myStrB = 'silent'
+    myStrA = 'william'
+    myStrB = 'mailwil'
     print('Anagram:', isAnagram(myStrA, myStrB))
 
 
